6b5598f40c60d2cca20319f5e9ca7603417d1ac3
Anton Yarkov <byslash@gmail.com>
2026-01-26 01:26:35 +0100

    Fix

--- chRUSTmas/src/lib.rs
+++ chRUSTmas/src/lib.rs
@@ -275,6 +275,7 @@ pub fn linear_backward_activation(
 
 // ------------------------------------- End of Backward Activations / Backward propagation functions below -------------------------------------
 
+#[derive(Clone, Debug)]
 pub struct DeepNeuralNetwork {
     pub layers: Vec<usize>,
     pub learning_rate: f32,
